b2754694403e51f707f42e39699f1ef86ef65330
juangovi <65275993+juangovi@users.noreply.github.com>
2022-04-06 19:19:20 +0200

    Initial commit

---
+++ src/main/java/com/juanantonio/demo/MyGeneralApplication.java
@@ -0,0 +1,13 @@
+package com.juanantonio.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyGeneralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyGeneralApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/juanantonio/demo/MyGeneralApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juanantonio.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyGeneralApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
